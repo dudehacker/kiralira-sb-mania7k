c5ec4b068f6f466043128a819b06048b7285bf1f
dudehacker <dudehacker>
2024-07-02 23:59:09 -0400

    add some efffects on snow scene before kiai 2

--- Background.cs
+++ Background.cs
@@ -9,12 +9,17 @@ namespace StorybrewScripts
         [Group("Timing")]
         [Configurable] public int StartTime = 0;
         [Configurable] public int EndTime = 0;
-        
+
         [Group("Sprite")]
         [Description("Leave empty to automatically use the map's background.")]
         [Configurable] public string SpritePath = "";
         [Configurable] public double Opacity = 0.2;
         [Configurable] public int fadeDuration = 500;
+        [Configurable] public double scale = 1.0;
+        [Configurable] public double endScale = 1.0;
+        [Configurable] public double scrollX = 0;
+        [Configurable] public double scrollY = 0;
+        [Configurable] public double rotationRad = 0;
 
         public override void Generate()
         {
@@ -23,9 +28,17 @@ namespace StorybrewScripts
 
             var bitmap = GetMapsetBitmap(SpritePath);
             var bg = GetLayer("Background").CreateSprite(SpritePath, OsbOrigin.Centre);
-            bg.Scale(StartTime, 480.0f / bitmap.Height);
+            bg.Scale(StartTime, EndTime, scale * 480.0f / bitmap.Height, endScale * 480.0f / bitmap.Height);
+            if (rotationRad != 0)
+            {
+                bg.Rotate(StartTime, EndTime, rotationRad, 0);
+            }
             bg.Fade(StartTime - fadeDuration, StartTime, 0, Opacity);
             bg.Fade(EndTime, EndTime + fadeDuration, Opacity, 0);
+            if (scrollX != 0 || scrollY != 0)
+            {
+                bg.Move(StartTime, EndTime, 320, 240, 320 + scrollX, 240 + scrollY);
+            }
         }
     }
 }
